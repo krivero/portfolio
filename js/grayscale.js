aba8e96c12c24c21843db1ecc1253c2ee458d52d
Kristina Rivero <kristinarivero@gmail.com>
2018-09-20 18:37:58 -0400

    add effect on main page

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,3 +1,46 @@
+var pos = 0;
+var speed = 75;
+var str = document.getElementById('str').innerText;
+
+// empty the str from span
+document.getElementById('str').innerText = '';
+
+// typing fn
+function type() {
+  if(pos<str.length) {
+    document.getElementById('str').innerText += str.charAt(pos);
+    pos++;
+    setTimeout(type,speed); //call this fn again to type all letters
+  } else {
+    setTimeout(erase,speed);
+  }
+}
+
+// start type
+setTimeout(type,speed);
+
+// erase fn
+function erase() {
+  if(pos>=0) {
+    var temp = str.substring(0, pos);
+    document.getElementById('str').innerText = temp;
+    pos--;
+    setTimeout(erase,speed);
+  } else {
+    // start type again
+    setTimeout(type,speed);
+  }
+}
+
+
+
+
+
+
+
+
+
+
 (function($) {
   "use strict"; // Start of use strict
 
